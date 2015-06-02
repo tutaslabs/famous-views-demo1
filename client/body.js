1e8453e40f825dd5d3a19b46523c3aafd7720e6b
tutasg@gmail.com <tutasg@gmail.com>
2015-06-01 23:24:48 -0400

    first commit

---
+++ client/body.js
@@ -0,0 +1,11 @@
+Template.body.helpers({
+    'bc1': function() {
+        return new famous.utilities.Color('lightblue');
+    },
+    'bc2': function() {
+        return new famous.utilities.Color('red');
+    },
+    'bc3': function() {
+        return new famous.utilities.Color('lightblue').setOpacity(0);
+    }
+})
